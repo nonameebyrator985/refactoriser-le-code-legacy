ba2e94c0c1c190d7978567849419e3c857f1767f
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-26 20:43:37 +0900

    Update command descriptions for clarity

--- cli.js
+++ cli.js
@@ -5,16 +5,16 @@ const { analyze, refactor } = require('./src/tools');
 
 program
   .command('analyze')
-  .option('-p, --path <path>', 'path to the project for analysis')
-  .option('-v, --verbose', 'increase verbosity of output')
+  .option('-p, --path <path>', 'Specify the path to the project for analysis')
+  .option('-v, --verbose', 'Enable verbose output for detailed information')
   .action((options) => {
     analyze(options.path, options.verbose);
   });
 
 program
   .command('refactor')
-  .option('-p, --path <path>', 'path to the project for refactoring')
-  .option('-v, --verbose', 'increase verbosity of output')
+  .option('-p, --path <path>', 'Specify the path to the project for refactoring')
+  .option('-v, --verbose', 'Enable verbose output for detailed information')
   .action((options) => {
     refactor(options.path, options.verbose);
   });
