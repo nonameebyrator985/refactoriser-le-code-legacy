58ac16af01cb10351f065d278c4e5db354d8aada
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-26 20:43:51 +0900

    Use arrow function shorthand for action handlers

--- cli.js
+++ cli.js
@@ -7,16 +7,12 @@ program
   .command('analyze')
   .option('-p, --path <path>', 'Specify the path to the project for analysis')
   .option('-v, --verbose', 'Enable verbose output for detailed information')
-  .action((options) => {
-    analyze(options.path, options.verbose);
-  });
+  .action(options => analyze(options.path, options.verbose));
 
 program
   .command('refactor')
   .option('-p, --path <path>', 'Specify the path to the project for refactoring')
   .option('-v, --verbose', 'Enable verbose output for detailed information')
-  .action((options) => {
-    refactor(options.path, options.verbose);
-  });
+  .action(options => refactor(options.path, options.verbose));
 
 program.parse(process.argv);
