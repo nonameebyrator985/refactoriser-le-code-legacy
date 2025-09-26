1b524014b4c6b7e6ca786a14b9f76609ccafcc6f
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-26 20:43:08 +0900

    Updated analyze and refactor command options to include verbosity level.

--- cli.js
+++ cli.js
@@ -6,15 +6,17 @@ const { analyze, refactor } = require('./src/tools');
 program
   .command('analyze')
   .option('-p, --path <path>', 'path to the project for analysis')
+  .option('-v, --verbose', 'increase verbosity of output')
   .action((options) => {
-    analyze(options.path);
+    analyze(options.path, options.verbose);
   });
 
 program
   .command('refactor')
   .option('-p, --path <path>', 'path to the project for refactoring')
+  .option('-v, --verbose', 'increase verbosity of output')
   .action((options) => {
-    refactor(options.path);
+    refactor(options.path, options.verbose);
   });
 
 program.parse(process.argv);
